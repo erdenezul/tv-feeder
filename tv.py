236e16a2a97e72aa4c9702801597e5015cabf32c
ez <zulaa@pyc.mn>
2010-10-29 10:23:44 +0800

    commit

--- tv.py
+++ tv.py
@@ -5,14 +5,33 @@ from models import Channel, ChannelEncoder
 from crawler import *
 import logging
 
+is_modified = True
+cached_response = ""
+
 class TVFeed(webapp.RequestHandler):
     def get(self):
-        json_encoder = ChannelEncoder(indent = 4)
-        channels = list(Channel.all())
-        self.response.out.write(json_encoder.encode(channels))
+        global is_modified, cached_response
+        if is_modified:
+            response = ""
+            for c in Channel.all():
+                response += c.name + '#' + c.img_url + '\n'
+                for i in range(1,8):
+                    programs = Program.gql("WHERE day = :day AND channel = :channel", day = i, channel = c)
+                    response += '\t' + str(i) + '\n'
+                    for p in programs:
+                        response += '\t\t' + p.time + '#' + p.name + '\n'
+            cached_response = response
+            is_modified = False
+            logging.info('cached response')
+            self.response.out.write(response)
+        else:
+            logging.info('responding from cache')
+            self.response.out.write(cached_response)
 
 class RunCrawler(webapp.RequestHandler):
     def get(self):
+        global is_modified
+        is_modified = True
         for ch in Channel.all():
             ch.delete()
         for c in CHANNELS_LIST:
@@ -28,7 +47,7 @@ class RunCrawler(webapp.RequestHandler):
         updateInfo(key, gogo_id)
 
 application = webapp.WSGIApplication(
-    [('/tvfeed/json', TVFeed),('/tvfeed/update', RunCrawler)],
+    [('/tvfeed/json', TVFeed),('/tvfeed/update', RunCrawler),],
     debug=True)
 
 def main():
